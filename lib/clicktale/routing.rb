b7cc4c712a7609136867194800b882d34d89989e
Brendon Murphy <xternal1+github@gmail.com>
2011-03-04 11:22:52 -0800

    Re-enabled routing mapping.

--- lib/clicktale/routing.rb
+++ lib/clicktale/routing.rb
@@ -1,11 +1,11 @@
-# module Clicktale #:nodoc:
-#   module Routing #:nodoc:
-#     module MapperExtensions
-#       def clicktales
-#         @set.add_route("/clicktales/:id", {:controller => "clicktales", :action => "show"})
-#       end
-#     end
-#   end
-# end
-#  
-# ActionController::Routing::RouteSet::Mapper.send :include, Clicktale::Routing::MapperExtensions
+module Clicktale #:nodoc:
+  module Routing #:nodoc:
+    module MapperExtensions
+      def clicktales
+        @set.add_route("/clicktales/:id", {:controller => "clicktales", :action => "show"})
+      end
+    end
+  end
+end
+ 
+ActionController::Routing::RouteSet::Mapper.send :include, Clicktale::Routing::MapperExtensions
