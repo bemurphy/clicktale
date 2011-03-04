a700e9dfdc0068a5f7cce9231d338f8333482230
Brendon Murphy <xternal1+github@gmail.com>
2011-03-04 11:43:48 -0800

    trying to get setup for rspec.

--- spec/clicktail_spec.rb
+++ spec/clicktail_spec.rb
@@ -1,17 +1,23 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Clicktale do
-  describe "controller" do
+  describe "controller methods" do
+    # Object.send(:remove_const, :MockController) rescue nil
+    # class MockClicktalesController < ApplicationController
+    #   include Clicktale::Controller
+    # end
+    # controller_name :mock_clicktales
   end
 
-  describe "helper" do
+  describe "helper methods" do
+    
   end
 
   describe "routing" do
     it "should route /clicktales/:id to the clicktales controller show action" do
-      pending
       ActionController::Routing::Routes.draw { |map| map.clicktales }
-      assert_routing("/clicktales/foo", { :controller => "clicktales", :action => "show" })
+      routes = ActionController::Routing::Routes
+      routes.recognize_path("/clicktales/foo").should == { :controller => "clicktales", :action => "show", :id => "foo" }
     end
   end
 end
